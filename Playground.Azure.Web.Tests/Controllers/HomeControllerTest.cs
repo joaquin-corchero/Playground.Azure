773535c78d133b3d988792b059a7a3dc918f0f32
Joaquin Corchero <joaquin.corchero@rewardinsight.com>
2015-07-01 13:00:04 +0100

    Broke a test

--- Playground.Azure.Web.Tests/Controllers/HomeControllerTest.cs
+++ Playground.Azure.Web.Tests/Controllers/HomeControllerTest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Playground.Azure.Web;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Playground.Azure.Web.Controllers;
+using System.Web.Mvc;
 
 namespace Playground.Azure.Web.Tests.Controllers
 {
@@ -35,7 +30,7 @@ namespace Playground.Azure.Web.Tests.Controllers
             ViewResult result = controller.About() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            Assert.AreEqual("----Your application description page.", result.ViewBag.Message);
         }
 
         [TestMethod]
