3324c5763d2007b4d93b2e2b04601eaacd7e2bd9
Joaquin Corchero <joaquin.corchero@rewardinsight.com>
2015-07-01 10:34:46 +0100

    Added web app

---
+++ Playground.Azure.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Playground.Azure.Web.Startup))]
+namespace Playground.Azure.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
