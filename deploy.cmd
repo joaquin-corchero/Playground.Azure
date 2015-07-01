38704d45205fc5e4781c8577a17acbed36b43bae
Joaquin Corchero <joaquin.corchero@rewardinsight.com>
2015-07-01 12:56:03 +0100

    Added comments for the building of the test project

--- deploy.cmd
+++ deploy.cmd
@@ -88,8 +88,9 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 
 :: ----------
 :: ----------
-::Run unit tests
+::Build unit test project
 call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\Playground.Azure.Web.Tests\Playground.Azure.Web.Tests.csproj" /t:rebuild /verbosity:m
+::Run unit tests
 vstest.console.exe "%DEPLOYMENT_SOURCE%\Playground.Azure.Web.Tests\bin\Debug\Playground.Azure.Web.Tests.dll"
 ::Finished running unit tests
 :: ----------
